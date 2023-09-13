0b553885ccbc1227eaa955b5e6d67dc0932b0dc1
Kiera <11018737@uvu.edu>
2023-09-13 10:48:07 -0600

    fixed spacing

--- Challenge.cs
+++ Challenge.cs
@@ -1,36 +1,54 @@
-{Console.WriteLine("The Temperature Adviser:")}
-{Console.WriteLine("What is the current temperature in Celsius?")}
+{
+    Console.WriteLine("The Temperature Adviser:")
+}
+{
+    Console.WriteLine("What is the current temperature in Celsius?")
+}
 
 int temp = 15;
 if (temp < 10) 
-{Console.WriteLine("It is recommended that you wear warm clothing!");}
+{
+    Console.WriteLine("It is recommended that you wear warm clothing!");
+}
  
 else if (temp >=10 and temp <=25)
-{Console.WriteLine("It is recommended that you carry a light jacket.");}
+{
+    Console.WriteLine("It is recommended that you carry a light jacket.");
+}
 
 else
-{Console.WriteLine("Enjoy the weather!")}
+{
+    Console.WriteLine("Enjoy the weather!")
+}
 
 
 
 
 
-{Console.WriteLine("Exam Grader:"):}
+Console.WriteLine("Exam Grader:"):
 
 int grade = 90;
 
 if (grade > 89) 
-{Console.WriteLine("A grade received!");} 
+{
+    Console.WriteLine("A grade received!");
+} 
 
 else if (grade < 80) 
-{Console.WriteLine("Below B grade received.");} 
+{
+    Console.WriteLine("Below B grade received.");
+} 
 
 else 
-{Console.WriteLine("Grade received!");}
+{
+    Console.WriteLine("Grade received!");
+}
 
 
 
-{Console.WriteLine("What's your favorite subject?:"):}
+{
+    Console.WriteLine("What's your favorite subject?:"):
+}
 
 string favsubject = Console.ReadLine();
 
