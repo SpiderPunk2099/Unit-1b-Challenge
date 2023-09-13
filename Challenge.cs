2edacb168217e61ac39af483164ff7b7d485ce61
Kiera <11018737@uvu.edu>
2023-09-13 10:35:48 -0600

    Completed Challenge 2: Exam Grader

--- Challenge.cs
+++ Challenge.cs
@@ -1,5 +1,5 @@
-print("The Temperature Adviser:")
-print("What is the current temperature in Celsius?")
+{Console.WriteLine("The Temperature Adviser:")}
+{Console.WriteLine("What is the current temperature in Celsius?")}
 
 int temp = 15;
 if (temp < 10) 
@@ -9,23 +9,44 @@ else if (temp >=10 and temp <=25)
 {Console.WriteLine("It is recommended that you carry a light jacket.");}
 
 else
-{print("Enjoy the weather!")}
+{Console.WriteLine("Enjoy the weather!")}
 
 
 
 
 
-
-
-print("Your Grade:")
+{Console.WriteLine("Exam Grader:"):}
 
 int grade = 90;
+
 if (grade > 89) 
-
 {Console.WriteLine("A grade received!");} 
+
 else if (grade < 80) 
-
 {Console.WriteLine("Below B grade received.");} 
-else 
 
+else 
 {Console.WriteLine("Grade received!");}
+
+
+
+{Console.WriteLine("What's your favorite subject?:"):}
+
+string favsubject = Console.ReadLine();
+
+switch (favsubject)
+{
+
+case "Math":
+    Console.WriteLine("Keep up the great work mathlete!")
+
+case "Art":
+    Console.WriteLine("Keep being unique, you got this!")
+
+case "History":
+    Console.WriteLine("Keep up the great work soldier!")
+
+case "English":
+    Console.WriteLine("You got this Shakespeare!")
+
+}
