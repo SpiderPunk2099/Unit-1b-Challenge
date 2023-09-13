7f436e2585d4ba73a69031cbaab07a993c42561c
Kiera <11018737@uvu.edu>
2023-09-13 09:40:52 -0600

    Completed Challenge 1: Weather Adviser

---
+++ Challenge.cs
@@ -0,0 +1,31 @@
+print("The Temperature Adviser:")
+print("What is the current temperature in Celsius?")
+
+int temp = 15;
+if (temp < 10) 
+{Console.WriteLine("It is recommended that you wear warm clothing!");}
+ 
+else if (temp >=10 and temp <=25)
+{Console.WriteLine("It is recommended that you carry a light jacket.");}
+
+else
+{print("Enjoy the weather!")}
+
+
+
+
+
+
+
+print("Your Grade:")
+
+int grade = 90;
+if (grade > 89) 
+
+{Console.WriteLine("A grade received!");} 
+else if (grade < 80) 
+
+{Console.WriteLine("Below B grade received.");} 
+else 
+
+{Console.WriteLine("Grade received!");}
